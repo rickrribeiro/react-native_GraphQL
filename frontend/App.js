fd07cc09ec94202b82689fece41dd9cac6b01669
rickrribeiro <rickribeiro99@gmail.com>
2020-12-13 11:40:39 -0300

    organizing project

---
+++ frontend/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight,Button,Alert, Platform } from 'react-native';
+
+export default function App() {
+
+  const handlePress =  ()=>console.log("clickeds");
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <Image source={require('./assets/icon.png')}/>
+      <TouchableHighlight onPress={handlePress}>
+        <Button title="login" onPress={() => Alert.alert("Senha Incorreta!","Caso tenha perdido sua senha, entre em contado com o administrador do sistema.",[{text:"yes", onPress: ()=> console.log("yes")},{text:"No"}])} ></Button>
+      </TouchableHighlight>
+      <View style={{
+        backgroundColor: 'red',
+        width: "50%",
+        height:70
+      }}>
+        </View>  
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Platform.OS === "android"? 20 : 0
+  },
+});
